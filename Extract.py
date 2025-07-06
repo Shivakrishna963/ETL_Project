794a0dffe68c8747d4fd8c9aa3867e2e04355588
Shivakrishna Cheripally <142949844+Shivakrishna963@users.noreply.github.com>
2025-07-06 01:51:17 -0700

    Update Extract.py

--- Extract.py
+++ Extract.py
@@ -55,7 +55,7 @@ def upload_to_gcs(bucket_name, source_file_path, destination_blob_name):
     bucket = client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
     blob.upload_from_filename(source_file_path)
-    print(f"✅ Uploaded {source_file_path} to gs://{bucket_name}/{destination_blob_name}")
+    print(f" Uploaded {source_file_path} to gs://{bucket_name}/{destination_blob_name}")
 
 if __name__ == "__main__":
     upload_to_gcs(
